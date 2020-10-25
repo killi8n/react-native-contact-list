24d75ff5afb44a308dbd54543df21506d7cdbc90
killi8n <killi8n.dev@gmail.com>
2020-10-25 16:02:21 +0900

    feat: example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,76 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import ContactList from 'react-native-contact-list';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Button,
+  FlatList,
+  Text,
+  SafeAreaView,
+} from 'react-native';
+import ContactList, { Contact } from 'react-native-contact-list';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    ContactList.multiply(3, 7).then(setResult);
-  }, []);
-
+  const [contactList, setContactList] = React.useState<Contact[]>([]);
+  const getContactList = async () => {
+    try {
+      const isPermissionAuthorized = await ContactList.checkPermission();
+      if (isPermissionAuthorized === 'authorized') {
+        setContactList(await ContactList.getContactList());
+      } else {
+        const permissionResult = await ContactList.requestPermission();
+        if (permissionResult === 'authorized') {
+          setContactList(await ContactList.getContactList());
+        } else {
+          // todo: PERMISSION DENIED EXCEPTION
+          console.log('todo: PERMISSION DENIED EXCEPTION');
+        }
+      }
+    } catch (e) {
+      console.error();
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Get Contact List" onPress={getContactList} />
+      <FlatList
+        data={contactList}
+        keyExtractor={(item, index) => `${item.phoneNumber}-${index}`}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.item}>
+              <Image
+                style={styles.thumbnailImage}
+                source={{
+                  uri: item.thumbnailPath ? item.thumbnailPath : '',
+                }}
+              />
+              <View style={styles.textWrapper}>
+                <Text style={styles.displayName}>{item.displayName}</Text>
+                <Text>{item.phoneNumber}</Text>
+              </View>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={styles.seperator} />}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  item: { flexDirection: 'row', alignItems: 'center' },
+  thumbnailImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    borderWidth: 1,
+    borderColor: '#000000',
+  },
+  textWrapper: { marginLeft: 10 },
+  displayName: { fontWeight: 'bold' },
+  seperator: { height: 15 },
 });
